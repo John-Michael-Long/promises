1b2710975f1ab341848c2bcf422dd3579ea3b4ef
John M. Long <johnm.long@Johns-MacBook-Pro.local>
2018-05-18 23:16:17 -0700

    passed all tests for all min requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,50 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(readFilePath, function(err, data) {
+      
+      if(err) return reject(err);
+      data = data.toString().split('\n').shift()
+      
+      resolve(data);
+    });
+  })
+  .then( (user) => { 
+
+    return new Promise(function(resolve, reject) {
+
+    var options = {
+      url: 'https://api.github.com/users/' + user,
+      headers: { 'User-Agent': 'request' },
+      json: true  // will JSON.parse(body) for us
+    };
+
+    request.get(options, function(err, res, body) {
+      if (err) return reject(err);
+
+      body = JSON.stringify(body)
+
+      resolve(body)
+      })
+    })
+  })
+  .then( (body) => { 
+
+    return new Promise(function(resolve, reject) {
+
+      fs.writeFile(writeFilePath, body, function(err, data){
+        if(err) return reject(err);
+        resolve(data)
+      })
+    })
+  });
 };
 
 // Export these functions so we can test them
